5b9aabf5ba04dbc7eaee13e4f4dc44f32b8fb8e6
KimHyunJun <proress@naver.com>
2025-12-05 13:51:27 +0900

    feat(FW) : Modify LoadingBar

--- Assets/2_Scripts/Framework/Patch/PatcherUI.cs
+++ Assets/2_Scripts/Framework/Patch/PatcherUI.cs
@@ -120,7 +120,7 @@ namespace LUP
                     }
                 }
 
-                yield return new WaitForSeconds(0.25f);
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
