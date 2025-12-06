07de8b9e2399a2c67768e2fb4c4f5fc00cf21ef6
JHLee <qnvudch03@naver.com>
2025-12-06 15:19:26 +0900

    feat(RL) : Apply Animation To Type Characters

--- Assets/2_Scripts/Games/RL/BehaviorTree/PlayerNode/PlayerActionNode.cs
+++ Assets/2_Scripts/Games/RL/BehaviorTree/PlayerNode/PlayerActionNode.cs
@@ -36,6 +36,7 @@ namespace LUP.RL
             if (bt.GetCurrentAnimState().IsName("Attack") == false)
             {
                 //bb.Shooter.TurnToTarget();
+                RotateHelper.LookAtTarget(bb.Health.transform, target.TargetPoint.transform, 8f);
                 bt.PlayAnimation(ActionState.Attack, this);
             }
             return NodeState.Success;
@@ -48,7 +49,7 @@ namespace LUP.RL
 
         public override void OnAnimationInTargetRate()
         {
-            RotateHelper.LookAtTarget(bb.Health.transform, target.TargetPoint.transform, 8f);
+            //RotateHelper.LookAtTarget(bb.Health.transform, target.TargetPoint.transform, 8f);
             bb.Shooter.TryShoot(target.TargetPoint.transform, bb.Health.Adata.currentData.Attack);
             lastFireTime = Time.time;
         }
