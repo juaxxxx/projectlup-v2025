8d261bab07266a5c2f157a30c8a034985231605b
a-i-am <sien6918@daum.net>
2026-01-09 07:54:23 +0900

    feat(PCR) : Edit APathfinding
    수정된 코드 추가

--- Assets/2_Scripts/Games/PCR/6_Worker/AStar/APathfinding.cs
+++ Assets/2_Scripts/Games/PCR/6_Worker/AStar/APathfinding.cs
@@ -14,10 +14,24 @@ namespace LUP.PCR
 
         int GetDistance(ANode a, ANode b)
         {
-            int dx = Mathf.Abs(a.indexX - b.indexX);
-            int dy = Mathf.Abs(a.indexY - b.indexY);
+            int distX = Mathf.Abs(a.indexX - b.indexX);
+            int distY = Mathf.Abs(a.indexY - b.indexY);
 
-            return 10 * (dx + dy); // 맨해튼 거리
+            int cost = 10 * (distX + distY);
+
+            // 엘리베이터끼리는 비용 대폭 할인 (선호도 증가)
+            if (a.isElevator && b.isElevator)
+            {
+                return (int)(cost * 0.5f);
+            }
+
+            // 사다리끼리는 비용 소폭 증가 (엘리베이터가 있다면 그걸 타도록 유도)
+            if (a.isLadder && b.isLadder)
+            {
+                return (int)(cost * 1.2f);
+            }
+
+            return cost;
         }
 
         public List<ANode> FindPath(ANode startNode, ANode targetNode)
@@ -84,27 +98,39 @@ namespace LUP.PCR
             {
                 for (int y = -1; y <= 1; y++)
                 {
-                    if (Mathf.Abs(x) + Mathf.Abs(y) != 1)
-                        continue; // 대각선 제외
-                    
+                    if (x == 0 && y == 0) { continue; }
+                    if (Mathf.Abs(x) + Mathf.Abs(y) != 1) { continue; } // 대각선 제외
+
                     int checkX = node.indexX + x;
                     int checkY = node.indexY + y;
 
-                    if (checkX >= 0 && checkX < gridMap.grid.GetLength(0)
-                    && checkY >= 0 && checkY < gridMap.grid.GetLength(1))
+                    if (gridMap.IsIdxValid(checkX, checkY))
                     {
-                        neighbors.Add(gridMap.grid[checkX, checkY]);
+                        ANode neighbor = gridMap.grid[checkX, checkY];
+                        if (!neighbor.isWalkable)
+                        {
+                            continue; // 벽이면 통과 }
+
+                            if (y == 0)
+                            {
+                                neighbors.Add(neighbor);
+                            }
+                            else
+                            {
+                                bool canClimb = (node.isLadder || neighbor.isLadder);
+                                bool canElevate = (node.isElevator || neighbor.isElevator);
+
+                                if (canClimb || canElevate)
+                                {
+                                    neighbors.Add(neighbor);
+                                }
+                            }
+                        }
                     }
 
-
                 }
-
             }
-
             return neighbors;
-
         }
-
     }
-
 }
