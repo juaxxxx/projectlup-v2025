b904b83f5561163819018195504fa7b9e708ea52
Jungdh1669 <jdh1669@naver.com>
2026-03-10 20:17:39 +0900

    feat(FW): add function
    애셋번들 수정, 애셋번들 전부 로드 가능

--- Assets/2_Scripts/Framework/StageLoadButton.cs
+++ Assets/2_Scripts/Framework/StageLoadButton.cs
@@ -6,7 +6,7 @@ public class StageLoadButton : MonoBehaviour
     public void LoadStage(int stage)
     {
         Debug.Log(stage);
-        LUP.Define.StageKind StageKind = (LUP.Define.StageKind)stage;
-        LUP.StageManager.Instance.GetCurrentStage().LoadStage(LUP.Define.StageKind.Main,stage);
+        LUP.Define.StageKind stageKind = (LUP.Define.StageKind)stage;
+        LUP.StageManager.Instance.GetCurrentStage().LoadStage(stageKind);
     }
 }
